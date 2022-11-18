f700a96c16fefad47231bedb9cfa00b1268c49f8
JuanLicciardi <juanelicciardi@gmail.com>
2022-11-18 00:48:31 -0300

    Finalizado Micro Desafio 03 - MoviesAdd.js

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,3 +1,21 @@
 console.log('Vinculacion externa Realizada MOVIESADD.JS')
 
      // MICRO DESAFIO 3
+
+             // Seleccion de Etiquetas h1, section y article
+             const h1 = document.querySelector('h1');
+             const section = document.querySelector('section');
+             const article = document.querySelector('article')
+     
+             // Se agrega a h1 texto
+             h1.innerHTML += 'AGREGAR PELÍCULAS'
+     
+             // Se agrega a h1 una clase
+             h1.classList.add('titulo')
+     
+             // Se agrega a article una clase
+             article.classList.add('fondoTransparente')
+     
+             // Se agrega a section una clase
+             section.classList.add('fondoCRUD')
+     
